0716ad505fc58b39c148de5bfd178ac048e8f34d
Qadrian <ianqadrian@gmail.com>
2019-03-19 14:51:02 +0700

    LatihanClassdanObject

---
+++ LatihanClassdanObject/LatihanClassdanObject/Mahasiswa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassdanObject
+{
+    public class Mahasiswa
+    {
+        //properties
+        public string NIM { get; set; }
+        public string Nama { get; set; }
+        public float IPK { get; set; }
+
+        //method
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama: {0}", Nama);
+            Console.WriteLine("NIM: {0}", NIM);
+            Console.WriteLine("IPK: {0}", IPK);
+            Console.WriteLine("Telah melakukan registrasi");
+
+        }
+
+        public void IsiKRS()
+        {
+            Console.WriteLine("{0} sedang melakukan proses KRS\n", Nama);
+        }
+    } 
+}
---
+++ LatihanClassdanObject/LatihanClassdanObject/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassdanObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Membuat objek dari class mahasiswa
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            //mengedit nilai propertis saya dan kamu
+            saya.NIM = "18.11.2308";
+            saya.Nama = "Qadrian Pristiadi";
+            saya.IPK = 4.00f;
+
+            kamu.NIM = "18.11.3222";
+            kamu.Nama = "Agus Wibowo";
+            kamu.IPK = 3.98f;
+
+            //mengambil method registrasi dan isis krs
+            saya.Registrasi();
+            saya.IsiKRS();
+
+            kamu.Registrasi();
+            kamu.IsiKRS();
+            Console.ReadKey();
+        }
+    }
+}
